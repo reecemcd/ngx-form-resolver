e0ec5be18a2e81b8af109aa59fd7b120d282782c
reecemcd <hayden@reecemcdonald.com>
2018-10-29 00:27:50 -0500

    Auto-generated commit

--- projects/ngx-form-resolver/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngx-fg-resolver
- */
-
-export * from './lib/form-resolver';
-export * from './lib/form-resolver.models';
-export * from './lib/form-resolver.resolvers';
-export * from './lib/form-resolver-builder.service';
-export * from './lib/form-resolver.module';
