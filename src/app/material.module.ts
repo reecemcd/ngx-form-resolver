54bdde96f3efd2aa4baa69f1c06a8663b8c84865
reecemcd <hayden@reecemcdonald.com>
2019-06-10 20:45:20 -0500

    updated angular dependencies

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatInputModule, MatCardModule, MatSelectModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MODULES = [
   MatButtonModule,
